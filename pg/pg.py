466216349e66521c8aa5201a4b7e2c97923a7f7f
yaeldingning <yaeldingning@didiglobal.com>
2024-02-24 23:49:21 +0800

    first version pg with no batch

--- pg/pg.py
+++ pg/pg.py
@@ -46,11 +46,33 @@ class PolicyGradient(nn.Module):
 
         # initialize networks, optimizers, move networks to device
         # BEGIN STUDENT SOLUTION
+
+        self.fc1 = nn.Linear(4, 10) # this is a 2 layer network, 10-middle layer, like a Q-table
+        nn.init.normal_(self.fc1.weight, 0, 0.3)
+        nn.init.constant_(self.fc1.bias, 0.1)
+
+        self.fc2 = nn.Linear(10, 2)
+        nn.init.normal_(self.fc2.weight, 0, 0.3)
+        nn.init.constant_(self.fc2.bias, 0.1)
+
         # END STUDENT SOLUTION
 
 
-    def forward(self, state):
-        return(self.actor(state), self.critic(state))
+    # def forward(self, state): # ori
+    #     return(self.actor(state), self.critic(state))
+
+    def forward_for_pg(self, x):
+        # convert numpy array to pytorch tensor
+        out = torch.from_numpy(x).float()
+
+        out = self.fc1(out)
+        out = F.sigmoid(out)
+
+        out = self.fc2(out)
+        scores = out
+        out = F.softmax(out, dim=0)
+
+        return out, scores
 
 
     def get_action(self, state, stochastic):
@@ -70,15 +92,83 @@ class PolicyGradient(nn.Module):
     def train(self, states, actions, rewards):
         # train the agent using states, actions, and rewards
         # BEGIN STUDENT SOLUTION
+        probs, scores = self.forward_for_pg(states)
+        log_prob = self.loss_func(scores, torch.tensor(actions))
+
+        # accumulating reward
+        acc_reward = []
+        for i in range(len(rewards)):
+            acc_r = 0
+            for j in range(i, len(rewards)):
+                acc_r += self.r_decay ** (j - i) * rewards[j]
+            acc_reward.append(acc_r)
+        acc_reward = torch.tensor(acc_reward)
+        acc_reward -= acc_reward.mean()
+        acc_reward /= acc_reward.std()  # todo check
+
+        log_reward = log_prob * acc_reward
+        loss = log_reward.mean()
+
+        self.optim.zero_grad()
+        loss.backward()
+        self.optim.step()
+
         # END STUDENT SOLUTION
         pass
+
+    def get_action_for_pg(self, probs):
+        action = np.random.choice(a=2, p=probs.detach().numpy())
+        return action
+
+    def policy(self, env, ob):
+        env.reset()
+
+        is_done = False
+        states = []
+        rewards = []
+        action_took = []
+
+        while (is_done != True):
+            # using current policy network to compute action
+            action_probs, _ = self.forward_for_pg(ob)
+            # print(action_probs)
+            cur_action = self.get_action_for_pg(action_probs)
+            # take the action & record
+            res = env.step(int(cur_action))
+            # print(res)
+            state, reward, done, info, _ = res  # dn
+            # add an additional dim for later concatenate
+            # print(state) # [ 0.04109825  0.1922446  -0.02631517 -0.33133468]
+            state_store = np.expand_dims(state, axis=0)  #?
+            # print(state_store) # [[ 0.04109825  0.1922446  -0.02631517 -0.33133468]]
+
+            env.render()
+            action_took.append(cur_action)
+            rewards.append(reward)
+            states.append(state_store)
+
+            # updates for next iteration
+            ob = state
+            is_done = done
+
+        return np.concatenate(states, axis=0), action_took, rewards
 
 
     def run(self, env, max_steps, num_episodes, train):
         total_rewards = []
-
         # run the agent through the environment num_episodes times for at most max steps
         # BEGIN STUDENT SOLUTION
+        self.r_decay = 0.99
+        lr = 0.02
+        self.optim = torch.optim.Adam(self.parameters(), lr=lr)
+        self.loss_func = nn.CrossEntropyLoss(reduction='none')
+        init_state = env.reset()
+        init_state = init_state[0]
+        for i_train in range(num_episodes): # todo minibatch /  5 IID trials?
+            states, actions, rewards = self.policy(env, init_state)
+            # print(states)
+            self.train(states, actions, rewards)  # learn
+            print("Episode {} is finished, with total rewards {} ...".format(i_train, torch.tensor(rewards).sum()))
         # END STUDENT SOLUTION
         return(total_rewards)
 
@@ -122,9 +212,16 @@ def parse_args():
 
 def main():
     args = parse_args()
+    max_steps = args.max_steps
+    num_episodes = args.num_episodes
+    train = "train"
 
     # init args, agents, and call graph_agents on the initialized agents
     # BEGIN STUDENT SOLUTION
+    pg_network = PolicyGradient(4, 2)
+    env = gym.make("CartPole-v1")
+    pg_network.run(env, max_steps, num_episodes, train)
+
     # END STUDENT SOLUTION
 
 
